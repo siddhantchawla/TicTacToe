f8b1161f0b2bcf3fbf2b400e99a50d70bb887503
Siddhant Chawla <siddhant.chawla9@gmail.com>
2021-05-25 14:20:24 +0530

    Update session settings

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ from game import moves,gameOver,tictactoe
 app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
